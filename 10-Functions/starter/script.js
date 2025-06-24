6a95234c2d17cfa82bcc12140f1aa06968ff738b
mikolaj-rellify <mikolaj@rellify.com>
2025-06-24 13:26:25 -0400

    Update script.js

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,44 @@
 'use strict';
+///////////////////////////////////////
+//Default Parameters
+// const bookings = [];
+
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 100 * numPassengers
+// ) {
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('A12', 6);
+
+//////////////////////////////////////
+//Passing Aguments: vlau vs reference
+
+const flight = 'A12';
+const miko = {
+  name: 'Miko syn',
+  passport: 123456789,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'A123';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 123456789) {
+    alert('check in');
+  } else {
+    alert('Wrong passport');
+  }
+};
+
+checkIn(flight, miko);
+console.log(flight);
+console.log(miko);

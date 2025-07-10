3108332d998ee2b5d72d3cbd0569b1b2cff9aabd
mikolaj-rellify <mikolaj@rellify.com>
2025-07-10 09:05:07 -0400

    d

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -3,12 +3,12 @@
 //Constructor functions
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
-  this.birtYear = birthYear;
+  this.birthYear = birthYear;
 };
 
 const miko = new Person('Miko', 2002);
 
-console.log(miko);
+// console.log(miko);
 // 1. new {} is created
 // 2. function is called, this = {}
 // 3. {} linked to prototype
@@ -16,15 +16,30 @@ console.log(miko);
 
 const filip = new Person('filip', 2003);
 
-const names = [miko, filip];
-console.log(names);
+// const names = [miko, filip];
+// console.log(names);
 
 //Prototypes
-
-Person.prototype.calclAge = function () {
-  console.log(2025 - this.birtYear);
+Person.prototype.calcAge = function () {
+  console.log(2025 - this.birthYear);
 };
 
-miko.calclAge();
+Person.prototype.hello = function () {
+  console.log(`Hello ${this.firstName}, you were born in ${this.birthYear}`);
+};
 
-console.log(miko.__proto__);
+miko.calcAge();
+miko.hello();
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(miko.species);
+
+console.log(miko.__proto__.__proto__);
+
+const arr = [1, 1, 2, 2, 3, 3, 4, 45, 55, 55];
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());

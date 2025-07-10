211247f810726dd964546bb0d2fdc654cf3ac7f4
mikolaj-rellify <mikolaj@rellify.com>
2025-07-10 09:43:59 -0400

    d

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,45 +1,45 @@
 'use strict';
 ////////////////////////////
 //Constructor functions
-const Person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
-};
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// };
 
-const miko = new Person('Miko', 2002);
+// const miko = new Person('Miko', 2002);
 
-// console.log(miko);
-// 1. new {} is created
-// 2. function is called, this = {}
-// 3. {} linked to prototype
-// 4. {} function automatically return {}
+// // console.log(miko);
+// // 1. new {} is created
+// // 2. function is called, this = {}
+// // 3. {} linked to prototype
+// // 4. {} function automatically return {}
 
-const filip = new Person('filip', 2003);
+// const filip = new Person('filip', 2003);
 
-// const names = [miko, filip];
-// console.log(names);
+// // const names = [miko, filip];
+// // console.log(names);
 
-//Prototypes
-Person.prototype.calcAge = function () {
-  console.log(2025 - this.birthYear);
-};
+// //Prototypes
+// Person.prototype.calcAge = function () {
+//   console.log(2025 - this.birthYear);
+// };
 
-Person.prototype.hello = function () {
-  console.log(`Hello ${this.firstName}, you were born in ${this.birthYear}`);
-};
+// Person.prototype.hello = function () {
+//   console.log(`Hello ${this.firstName}, you were born in ${this.birthYear}`);
+// };
 
-miko.calcAge();
-miko.hello();
+// miko.calcAge();
+// miko.hello();
 
-Person.prototype.species = 'Homo Sapiens';
-console.log(miko.species);
+// Person.prototype.species = 'Homo Sapiens';
+// console.log(miko.species);
 
-console.log(miko.__proto__.__proto__);
+// console.log(miko.__proto__.__proto__);
 
-const arr = [1, 1, 2, 2, 3, 3, 4, 45, 55, 55];
+// const arr = [1, 1, 2, 2, 3, 3, 4, 45, 55, 55];
 
-Array.prototype.unique = function () {
-  return [...new Set(this)];
-};
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
 
-console.log(arr.unique());
+// console.log(arr.unique());

6dc8a8dea530d87904de00b0ca4a9aa2b1d75819
mikolaj-rellify <mikolaj@rellify.com>
2025-07-11 12:19:07 -0400

    d

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -10,3 +10,8 @@ const countriesContainer = document.querySelector('.countries');
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.com/v2/name/portugal');
+request.send();
+request.addEventListener(')
+    
